2cddb6f77b0e1ed6a16cc9db569e4371f3e96611
fliaping <xavierrpayne@gmail.com>
2017-10-30 22:20:28 +0800

    init

--- src/main/java/com/fliaping/proxy/h2/StartMain.java
+++ src/main/java/com/fliaping/proxy/h2/StartMain.java
@@ -1,8 +1,26 @@
 package com.fliaping.proxy.h2;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author payne
  */
 public class StartMain {
 
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        Socket clientSocket = serverSocket.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        String request, response;
+        while ((request = in.readLine()) != null) {
+            if ("Done".equals(request)) {
+                break;
+            }
+            response = request;
+            out.print(response);
+        }
+    }
 }
